b033a54c1cd71713a91a93ea64b0024e972e63c1
lahitha <vallurilahitha@gmail.com>
2026-06-16 00:53:35 +0530

    resolve deployment errors

--- server/app.js
+++ server/app.js
@@ -21,7 +21,11 @@ app.use(cors({
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname1, "client", "build", "index.html")
+    );
+  });
 }
 
 // DB
